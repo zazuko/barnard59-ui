0f943d858740abebad6a79748a8a88a48be2342a
tpluscode <tpluscode@users.noreply.github.com>
2019-02-25 11:47:13 +0100

    rename

--- test/store/pipeline-mutations.spec.js
+++ test/store/pipeline-mutations.spec.js
@@ -1,5 +1,43 @@
-/* global describe */
+import { PIPELINE_LOADED } from '../../src/store/pipeline-mutations'
+import { mutations } from '../../src/store/pipeline'
+import { expect } from 'chai'
 
-describe('', () => {
+describe('mutations', () => {
+  describe(PIPELINE_LOADED, () => {
+    const mutation = mutations[PIPELINE_LOADED]
 
+    it('sets the @graph object into state', () => {
+      // given
+      const state = {}
+      const graph = []
+
+      // when
+      mutation(state, {
+        '@graph': graph
+      })
+
+      // then
+      expect(state.resources).to.be.an('array')
+    })
+
+    it('sets the root resource as state instance', () => {
+      // given
+      const state = {
+        iri: 'http://example.resource'
+      }
+      const graph = [
+        {
+          id: 'http://example.resource'
+        }
+      ]
+
+      // when
+      mutation(state, {
+        '@graph': graph
+      })
+
+      // then
+      expect(state.instance.id).to.be.equal(state.iri)
+    })
+  })
 })
