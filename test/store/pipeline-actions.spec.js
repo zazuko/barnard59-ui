12c19f0267150a3f1e9ee404bafeb0ca8dc3421b
tpluscode <tpluscode@users.noreply.github.com>
2019-02-26 11:01:25 +0100

    test save action

--- test/store/pipeline-actions.spec.js
+++ test/store/pipeline-actions.spec.js
@@ -1,11 +1,12 @@
-import { expect } from 'chai'
-import { load } from '../../src/store/pipeline-action-types'
+import { expect, assert } from 'chai'
+import { load, save } from '../../src/store/pipeline-action-types'
 import * as mutations from '../../src/store/pipeline-mutation-types'
 import actions from '../../src/store/pipeline-actions'
 import * as sinon from 'sinon'
 import Client from '../../src/Client'
 import cf from 'clownface'
 import rdf from 'rdf-ext'
+import { code } from '../../src/utils/namespaces'
 
 describe('action', () => {
   describe(load, () => {
@@ -22,6 +23,70 @@ describe('action', () => {
 
       // then
       expect(commit.firstCall.args[0]).to.equal(mutations.IRI_SET)
+    })
+
+    it('commits the pipeline body to state', async () => {
+      // given
+      const client = sinon.mock(new Client())
+      const commit = sinon.spy()
+      const rootState = { client: client.object }
+      const iri = 'urn:pipeline:1'
+      client.expects('fetch').once().resolves(cf(rdf.dataset()))
+
+      // when
+      await actions.load({ commit, rootState }, iri)
+
+      // then
+      expect(commit.secondCall.args[0]).to.equal(mutations.PIPELINE_LOADED)
+      expect(commit.secondCall.args[1]['@graph']).to.be.an('array')
+    })
+  })
+
+  describe(save, () => {
+    it('saves the pipeline', async () => {
+      // given
+      const state = {
+        iri: 'urn:test:pipeline',
+        '@graph': {
+          'urn:test:pipeline': {
+            '@type': code.Pipeline
+          }
+        }
+      }
+      const dispatch = sinon.spy()
+      const client = sinon.mock(new Client())
+      const rootState = { client: client.object }
+      client.expects('update').once()
+
+      // when
+      await actions.save({ state, rootState, dispatch })
+
+      // then
+      client.verify()
+    })
+
+    it('reloads the pipeline', async () => {
+      // given
+      const state = {
+        iri: 'urn:test:pipeline',
+        '@graph': {
+          'urn:test:pipeline': {
+            '@type': code.Pipeline
+          }
+        }
+      }
+      const dispatch = sinon.spy()
+      const client = sinon.mock(new Client())
+      const rootState = { client: client.object }
+      client.expects('update').once()
+
+      // when
+      await actions.save({ state, rootState, dispatch })
+
+      // then
+      expect(dispatch.firstCall.args).to.deep.equal([
+        load, state.iri
+      ])
     })
   })
 })
