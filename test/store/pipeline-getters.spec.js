5910d58f270acd37934f30dc3ec5637302c6d14d
tpluscode <tpluscode@users.noreply.github.com>
2019-03-13 17:05:20 +0100

    add fallback value for variables getter

---
+++ test/store/pipeline-getters.spec.js
@@ -0,0 +1,45 @@
+import store from '../../src/store/pipeline'
+import { expect } from 'chai'
+
+const { getters } = store
+
+describe('pipeline getter', () => {
+  describe('variables', () => {
+    it('returns inner variables', () => {
+      // given
+      const state = {
+        instance: {
+          variables: [
+            {
+              variable: {
+                name: 'foo',
+                value: 'bar'
+              }
+            }
+          ]
+        }
+      }
+
+      // when
+      const variables = getters.variables(state)
+
+      // then
+      expect(variables[0].name).to.be.equal('foo')
+      expect(variables[0].value).to.be.equal('bar')
+    })
+
+    it('returns empty array if instance does not have variables', () => {
+      // give
+      const state = {
+        instance: {}
+      }
+
+      // when
+      const variables = getters.variables(state)
+
+      // then
+      expect(variables).to.be.an('array')
+      expect(variables.length).to.equal(0)
+    })
+  })
+})
