0327f3a146d8de322659c6812b02eb482fcc45f4
tpluscode <tpluscode@users.noreply.github.com>
2019-03-25 15:51:01 +0100

    test for load resource action

--- test/store/root-actions.spec.js
+++ test/store/root-actions.spec.js
@@ -1,7 +1,9 @@
 import { assert } from 'chai'
-import { SAVE_RESOURCE, ADD_RESOURCE } from '../../src/store/root/action-types'
+import { SAVE_RESOURCE, ADD_RESOURCE, LOAD_RESOURCE } from '../../src/store/root/action-types'
 import rdf from 'rdf-ext'
 import * as sinon from 'sinon'
+import cf from 'clownface'
+import { promises as jsonld } from 'jsonld'
 import Client from '../../src/Client'
 import actions from '../../src/store/root/actions'
 import * as mutations from '../../src/store/root/mutation-types'
@@ -44,6 +46,39 @@ describe('root store', () => {
           mutations.RESOURCE_ADDED,
           resource
         ))
+      })
+    })
+
+    describe(LOAD_RESOURCE, () => {
+      it('commits framed JSON-LD to state', async () => {
+        // given
+        const commit = sinon.spy()
+        const state = {
+          client: new Client()
+        }
+        const frame = {}
+        sinon.stub(state.client, 'fetch').callsFake(() => cf(rdf.dataset()))
+        const frameSpy = sinon.spy(jsonld, 'frame')
+        const getters = {
+          resourceIri: () => 'urn:test:id'
+        }
+
+        // when
+        await actions[LOAD_RESOURCE]({ state, getters, commit }, frame)
+
+        // then
+        assert(commit.calledWith(
+          mutations.RESOURCE_LOADED,
+          sinon.match({ '@graph': [] })
+        ))
+        assert(frameSpy.calledWith(
+          sinon.match.any,
+          frame
+        ))
+      })
+
+      afterEach(() => {
+        jsonld.frame.restore()
       })
     })
   })
